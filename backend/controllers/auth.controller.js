8c083bcbe81ed932eafa0d266cb42a4b70910e6c
Makay131 <mahmutenesakay9@gmail.com>
2024-08-26 14:09:28 +0300

    feat: login functionality added - be

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -51,7 +51,29 @@ export const signup = async (req, res) => {
 }
 
 export const login = async (req, res) => {
+    const { email, password } = req.body;
+    try {
+        const user = await User.findOne({ email });
+        if(!user) return res.status(400).json({ success: false, message: "No user found with the provided credentials" });
+        const isPasswordValid = await bcryptjs.compare(password, user.password);
+        if(!isPasswordValid) return res.status(400).json({ success: false, message: "Wrong password provided"});
 
+        generateTokenAndSetCookie(res, user._id);
+
+        user.lastLogin = new Date();
+        await user.save();
+
+        res.status(200).json({
+            success: true,
+            message: "Logged in successfully",
+            user: {
+                ...user._doc,
+                password: undefined,
+            }
+        })
+    } catch (error) {
+        res.status(400).json({ success: false, message: error.message});
+    }
 }
 
 export const logout = async (req, res) => {
