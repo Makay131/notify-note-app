71ad11241494200a079a6533dbba91ef2afc8d9a
Makay131 <mahmutenesakay9@gmail.com>
2024-09-02 16:40:17 +0300

    feat: auth guard for dashboard pages is added

--- frontend/src/routes/dashboard/route.tsx
+++ frontend/src/routes/dashboard/route.tsx
@@ -1,7 +1,18 @@
-import { createFileRoute, Outlet } from '@tanstack/react-router';
+import { createFileRoute, Outlet, redirect } from '@tanstack/react-router';
 import Sidebar from '../../components/sidebar/Sidebar';
+import { checkAuth } from '../../service/auth/api';
 
 export const Route = createFileRoute('/dashboard')({
+  beforeLoad: async ({ location }) => {
+    try {
+      await checkAuth();
+    } catch (error) {
+      console.log(error);
+      throw redirect({
+        to: '/login',
+      });
+    }
+  },
   component: DashboardPage,
 });
 
